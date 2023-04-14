86460ce88a9c6320d47821df2919f3ba8559d2b6
Александр <perchatki79@gmail.com>
2023-04-14 19:11:40 +0300

    Initial commit

---
+++ src/main/java/pro/sky/employee_stream/EmployeeStreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employee_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeStreamApplication.class, args);
+	}
+
+}
